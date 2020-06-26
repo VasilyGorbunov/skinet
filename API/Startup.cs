61058d400a66b7b6d453044ab93914014bf404b7
VasilyGorbunov <vasily.gorbunov@gmail.com>
2020-06-26 23:20:00 +0300

    68

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,12 +33,20 @@ namespace API
       services.AddControllers();
       services.AddDbContext<StoreContext>(opts =>
         opts.UseSqlite(_config.GetConnectionString("DefaultConnection"))
-      ); 
+      );
       services.AddAutoMapper(typeof(MappingProfiles));
 
       services.AddApplicationServices();
 
       services.AddSwaggerDocumentation();
+
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +61,8 @@ namespace API
       app.UseRouting();
 
       app.UseStaticFiles();
+
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
